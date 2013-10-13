988ce19ce7c32abb6490b5b3e2fdf02f8c2aec46
Alain Beauvois <alain@itipic.com>
2013-10-13 21:20:02 +0200

    Design login form and add basic validations on email and password

--- Victoria/src/com/itipic/victoria/LoginActivity.java
+++ Victoria/src/com/itipic/victoria/LoginActivity.java
@@ -1,10 +1,15 @@
 package com.itipic.victoria;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+	private String mUserEmail;
+	private String mUserPassword;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,22 @@ public class LoginActivity extends Activity {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
+	
+	public void login(View button) {
+	    EditText userEmailField = (EditText) findViewById(R.id.userEmail);
+	    mUserEmail = userEmailField.getText().toString();
+	    EditText userPasswordField = (EditText) findViewById(R.id.userPassword);
+	    mUserPassword = userPasswordField.getText().toString();
 
+	    if (mUserEmail.length() == 0 || mUserPassword.length() == 0) {
+	        // input fields are empty
+	        Toast.makeText(this, "Please complete all the fields",
+	            Toast.LENGTH_LONG).show();
+	        return;
+	    } else {
+	       // LoginTask loginTask = new LoginTask(LoginActivity.this);
+	        //loginTask.setMessageLoading("Logging in...");
+	        //loginTask.execute(LOGIN_API_ENDPOINT_URL);
+	    }
+	}
 }
